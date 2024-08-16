2cc54c706b4b7e0f179b5369a37a3fd0ef0ea27b
Felipe337 <116205272+Felipe337@users.noreply.github.com>
2024-08-16 13:02:13 -0400

    Create ordena.h
    Create ordena.h

---
+++ ordena.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void trocar(int *a, int *b);
+void imprimirVetor(int vetor[], int tamanho);
+void ordenacaoPorTroca(int vetor[], int tamanho);
+void ordenacaoPorSelecao(int vetor[], int tamanho);
+void ordenacaoPorInsercao(int vetor[], int tamanho);
+void ordenacaoPorMesclagem(int vetor[], int inicio, int fim);
